e69b9b9077e1d88d6c9b3eea82a7330f1a1296f4
eliasfrigard <elias_frigard@hotmail.com>
2023-01-21 13:12:30 +0200

    adding small videos

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,26 @@ export default function Home() {
         <Image alt='Mountains' src='/polenta-hero.jpg' layout='fill' objectFit='cover' />
         <div className='container h-[400px] bg-black absolute translate-y-[525px]'></div>
       </div>
-      <div className='py-16 pt-[264px] bg-green-500 flex justify-center items-center'>
+      <div className='py-16 pt-[264px] bg-green-500 flex justify-center items-center flex-col'>
         <div className='container'>
+          <iframe
+            className='w-full aspect-video'
+            src='https://www.youtube.com/embed/UpaJ4JEoxy8'
+            title='YouTube video player'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+            loading='lazy'
+          ></iframe>
+        </div>
+        <div className='container flex gap-16 mt-16 flex-wrap md:flex-nowrap'>
+          <iframe
+            className='w-full aspect-video'
+            src='https://www.youtube.com/embed/UpaJ4JEoxy8'
+            title='YouTube video player'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+            loading='lazy'
+          ></iframe>
           <iframe
             className='w-full aspect-video'
             src='https://www.youtube.com/embed/UpaJ4JEoxy8'
