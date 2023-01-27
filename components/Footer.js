40942c6b74d8c798eba130007af01afbbda1fccc
eliasfrigard <elias_frigard@hotmail.com>
2023-01-27 23:35:13 +0200

    footer padding mobile

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer({ height }) {
   return (
-    <div className='w-full h-[450px] bg-[#011C26] flex justify-center py-16'>
+    <div className='w-full h-[450px] bg-[#011C26] flex justify-center py-16 px-8 md:px-0'>
       <div className='container bg-[#592B02] h-full rounded'></div>
     </div>
   )
