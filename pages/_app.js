4c48e59eee8997a4267a5a6d287baf4c09c95253
Elias Frigård <elias.frigard@tvtools.fi>
2023-01-31 20:09:43 +0200

    custom script

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <script defer type="text/javascript" src="/static/script.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
+
+
 
 export default MyApp
