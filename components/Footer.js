05ea865dccbbf0ed87fcfcd05ac2d0b89fdce8dc
eliasfrigard <elias_frigard@hotmail.com>
2023-01-31 22:34:50 +0200

    footer x padding only mobile

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer({ height }) {
   return (
     <div className='w-full bg-secondary-500 flex justify-center py-8 px-8 md:px-0 text-primary-500'>
-      <div className='container grid grid-flow-row h-full rounded px-32 tracking-wide leading-loose'>
+      <div className='container grid grid-flow-row rounded md:px-32 tracking-wide leading-loose'>
         <div className='text-center'>
           <p>Copyright 2023 © Polenta Music</p>
           <p className='text-sm mt-2'>Website by Elias Frigård</p>
