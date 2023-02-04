b30d489c92bc6f8b0fbbfd0fafa2c3b8a0d16d84
eliasfrigard <elias_frigard@hotmail.com>
2023-02-04 15:08:22 +0200

    contact page from contentful

--- pages/contact.js
+++ pages/contact.js
@@ -7,33 +7,45 @@ import Title from '../components/Title'
 import DownloadItem from '../components/DownloadItem'
 import ContactForm from '../components/ContactForm'
 
-import md from 'markdown-it'
-import axios from 'axios'
+import { createClient } from 'contentful'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-export default function Contact() {
-  const [post, setPost] = React.useState('')
+export async function getStaticProps() {
+  const contentful = createClient({
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_TOKEN,
+  })
 
-  React.useEffect(() => {
-    const getPost = async () => {
-      const { data: post } = await axios.get('/exampleContact.md')
+  const pageRes = await contentful.getEntries({
+    content_type: 'contactPage',
+  })
 
-      setPost(post)
-    }
+  const page = pageRes.items[0].fields
 
-    getPost()
-  }, [])
+  return {
+    props: {
+      hero: page.heroImage,
+      text: page.contactInformation,
+    },
+  }
+}
 
+export default function Contact({ text, hero }) {
   return (
     <Layout>
       <div id='hero' className='relative h-screen centerContent shadow-xl'>
-        <Image alt='Mountains' src='/polenta-14.jpg' layout='fill' objectFit='cover' />
+        <Image
+          alt={hero.fields.title}
+          src={'https:' + hero.fields.file.url}
+          layout='fill'
+          objectFit='cover'
+        />
       </div>
 
       <div className='px-8 md:px-0 my-12 md:my-32 container flex justify-center'>
-        <div
-          className='-translate-x-[2px] prose prose-lg md:prose-xl max-w-4xl prose-img:rounded-xl prose-img:shadow-lg prose-headings:underline leading-[2rem] text-center'
-          dangerouslySetInnerHTML={{ __html: md().render(post) }}
-        />
+        <div className='-translate-x-[2px] prose prose-lg md:prose-xl max-w-4xl prose-img:rounded-xl prose-img:shadow-lg prose-headings:underline leading-[2rem] text-center'>
+          {documentToReactComponents(text)}
+        </div>
       </div>
 
       <div className='container my-12 md:my-32 flex justify-center items-center gap-8 md:gap-16 flex-wrap'>
