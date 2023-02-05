1d929866efdf168d6e3f01cfd8751242d12adeb9
eliasfrigard <elias_frigard@hotmail.com>
2023-02-05 22:14:17 +0200

    shaking some excess code to lose transform

--- components/AnimateIn.js
+++ components/AnimateIn.js
@@ -11,56 +11,21 @@ import { useInView } from 'react-intersection-observer'
 
 const AnimateIn = ({
   delay = 0,
-  distance = 30,
-  direction = 'up',
   threshold = 0.2,
   triggerOnce = true,
   opacityDuration = 400,
-  transformDuration = 400,
-  disabled = false,
   children,
   classes,
 }) => {
   const [ref, inView] = useInView({ threshold, triggerOnce })
 
-  // Variable for setting the direction of animation.
-  let translateDirection = direction
-
-  switch (direction) {
-    case 'up':
-    case 'down':
-      translateDirection = 'translateY'
-      break
-    case 'left':
-    case 'right':
-      translateDirection = 'translateX'
-      break
-  }
-
-  // Variable for setting the distance of animation.
-  let translateDistance = distance
-
-  switch (direction) {
-    case 'up':
-    case 'left':
-      translateDistance = Math.abs(distance)
-      break
-    case 'down':
-    case 'right':
-      translateDistance = -Math.abs(distance)
-      break
-  }
-
-  return disabled ? (
-    <>{children}</>
-  ) : (
+  return (
     <div
       className={classes}
       ref={ref}
       style={{
         transition: `opacity ${opacityDuration}ms`,
         transitionDelay: `${delay}ms`,
-        transform: `${translateDirection}(${inView ? 0 : translateDistance}px)`,
         opacity: inView ? 1 : 0,
       }}
     >
