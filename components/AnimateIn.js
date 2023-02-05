76533d1f8140faf390a9a522445b9f21a745f8af
eliasfrigard <elias_frigard@hotmail.com>
2023-02-05 22:20:52 +0200

    shaking some more animation code

--- components/AnimateIn.js
+++ components/AnimateIn.js
@@ -1,34 +1,11 @@
 import React from 'react'
 import { useInView } from 'react-intersection-observer'
 
-/**
- * @param distance = distance of the animation in pixels.
- * @param direction = the direction of the animation.
- * @param threshold = amount of pixels in view before effect starts.
- *
- * Set distance to 0 to get a simple fade-in.
- */
-
-const AnimateIn = ({
-  delay = 0,
-  threshold = 0.2,
-  triggerOnce = true,
-  opacityDuration = 400,
-  children,
-  classes,
-}) => {
+const AnimateIn = ({ threshold = 0.2, triggerOnce = true, children, classes }) => {
   const [ref, inView] = useInView({ threshold, triggerOnce })
 
   return (
-    <div
-      className={classes}
-      ref={ref}
-      style={{
-        transition: `opacity ${opacityDuration}ms`,
-        transitionDelay: `${delay}ms`,
-        opacity: inView ? 1 : 0,
-      }}
-    >
+    <div className={`${classes} duration-1000 ${inView ? 'opacity-100' : 'opacity-0'}`} ref={ref}>
       {children}
     </div>
   )
