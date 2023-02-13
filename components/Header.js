85c877f08986e628647280423a4dfe4293a8dbc1
eliasfrigard <elias_frigard@hotmail.com>
2023-02-13 19:22:15 +0200

    fix desktop nav onClick

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ export default function Header() {
 
   const activeLinkStyling = (path) => {
     if (router.pathname == path) {
-      return 'border-b-2 border-accent-500 border-opacity-70 hover:border-accent-500 hover:border-opacity-70'
+      return 'border-b-2 border-accent-500 border-opacity-70 hover:border-accent-500 hover:border-opacity-70 text-accent-500'
     }
   }
 
