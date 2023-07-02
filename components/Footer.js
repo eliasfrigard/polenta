2bc862f7a3645a6c15dc22bf81bb87d1e226bf32
Elias Frigård <elias.frigard@tvtools.fi>
2023-07-02 11:16:16 +0200

    always full year in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,18 @@
+import { useState, useEffect } from "react"
+
 export default function Footer() {
+  const [currentYear, setCurrentYear] = useState('')
+
+  useEffect(() => {
+    const year = new Date().getFullYear();
+    setCurrentYear(year.toString());
+  }, [])
+
   return (
     <div className='reverseBg absolute w-full flex justify-center py-8 px-8 md:px-0'>
       <div className='container grid grid-flow-row rounded md:px-32 tracking-wide leading-loose'>
         <div className='text-center'>
-          <p>Copyright 2023 © Polenta Music</p>
+          <p>Copyright {currentYear} © Polenta Music</p>
           <p className='text-xs mt-2'>Website by Elias Frigård</p>
         </div>
       </div>
